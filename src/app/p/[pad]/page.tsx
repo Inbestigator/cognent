bdf5f70ad477c55bc4e8be70b55523991827307e
Inbestigator <inbestigatoryt@gmail.com>
2024-11-23 01:08:00 -0800

    Metadata

--- src/app/p/[pad]/page.tsx
+++ src/app/p/[pad]/page.tsx
@@ -2,6 +2,41 @@ import { auth } from "@/server/auth";
 import { api } from "@/trpc/server";
 import Editor from "@/components/editor/editor";
 import { notFound } from "next/navigation";
+import { db } from "@/server/db";
+import type { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{
+    pad: string;
+  }>;
+}): Promise<Metadata> {
+  const pad = await db.pad.findFirst({
+    where: {
+      id: (await params).pad,
+    },
+    include: {
+      createdBy: true,
+    },
+  });
+
+  if (!pad) {
+    notFound();
+  }
+
+  return {
+    title: {
+      absolute: pad.name,
+    },
+    description: "",
+    authors: [{ name: `${pad.createdBy.name}` }],
+    openGraph: {
+      type: "article",
+      publishedTime: new Date(pad.createdAt).toISOString(),
+    },
+  };
+}
 
 export default async function PadPage({
   params,
@@ -14,7 +49,7 @@ export default async function PadPage({
   const pad = await api.pad.getPad((await params).pad);
 
   if (!pad) {
-    return notFound();
+    notFound();
   }
 
   return (
