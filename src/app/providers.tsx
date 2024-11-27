8e14f69ff2777937adad2b58c5b025b655165cde
Inbestigator <inbestigatoryt@gmail.com>
2024-11-26 16:03:44 -0800

    Added session to providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import * as React from "react";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
+    <NextThemesProvider defaultTheme="system" enableSystem attribute="class">
+      <SessionProvider>
+        {children}
+      </SessionProvider>
     </NextThemesProvider>
   );
 }
